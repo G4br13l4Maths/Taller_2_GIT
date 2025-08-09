a13538e388103a57642c31e57cfe7b610680920c
Gabriela Cortes <g.cortesm@uniandes.edu.co>
2025-08-09 16:12:44 -0500

    Ajuste menor en app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.title = "Dashboard energia"
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-
+# New comment 
 # Load data from csv
 def load_data():
     # To do: Completar la función 
